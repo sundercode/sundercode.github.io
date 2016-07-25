16f82376880268dbb73f3eb84ff76de76f02c020
Elise Sunderland <sunderla17@up.edu>
2016-07-25 11:50:44 -0700

    messing with making a comment box...

---
+++ src/client/app/commentBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className='CommentBox'>
+        Hello, World! I am a comment box.
+      </div>
+    );
+  }
+});
+
+export default CommentBox;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
+import CommentBox from './CommentBox.jsx';
 
 class App extends React.Component {
   render () {
@@ -9,6 +10,8 @@ class App extends React.Component {
         <p> Hello React!</p>
         <AwesomeComponent />
       </div>
+      <div>
+        <
     );
   }
 }
